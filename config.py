9bbb7e920bd8b9b6ede907cad42dac0641297375
Zaw Htet Aung <zawhtetaung.coms@gmail.com>
2025-08-30 01:59:41 +0630

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ APP_ID = int(os.environ.get("APP_ID", "23574009"))
 API_HASH = os.environ.get("API_HASH", "17e1e468e29fe266622955410bcc48f2")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003001037770"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002653852220"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "7726252322"))
@@ -30,7 +30,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://tksfamilytg:hQx57pZYGBRob
 DB_NAME = os.environ.get("DATABASE_NAME", "tksfamilytg")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002932116854"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002653852220"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
